a49e339e82832df67a0dc0c1957c3c0346e0459c
Shaunett <shanthedeveloper@gmail.com>
2019-08-29 23:04:55 -0400

    Add style images animation

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ function changeBackground(){
 
 (function(){
     setTimeout(changeBackground,3000);
+    setTimeout(changeToGreen,7000);
+
+    document.querySelector(".poppy-moving").style.WebkitAnimationName = "myNEWmove"; // Code for Chrome, Safari, and Opera
+    document.querySelector(".poppy-moving").style.animationName = "myNEWmove";
 })();
+
+
+function changeToGreen(){
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "#fff";
+}
